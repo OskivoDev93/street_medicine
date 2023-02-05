d94d3908e81a949763cef8f69e30834352ee9f5d
mujadidfhussain <mujadid@whipmobility.com>
2023-02-05 16:51:33 +0800

    first api db call

--- app/controllers/user-controller/user-controller.ts
+++ app/controllers/user-controller/user-controller.ts
@@ -1,20 +1,36 @@
 import { Request, Response } from "express";
-
-const getUsers = (req: Request, res: Response) => {
+const asyncHandler = require("express-async-handler");
+const User = require("../../models/user-model");
+import { v4 as uuidV4, v4 } from "uuid";
+const getUsers = asyncHandler(async (req: Request, res: Response) => {
+  const users = await User.find();
   res.status(200).json({ message: "get all USERS" });
-};
+});
 
 const getOneUser = (req: Request, res: Response) => {
   res.status(200).json({ message: `${req.params.id} retrieved` });
 };
 
-const createUser = (req: Request, res: Response) => {
+const createUser = asyncHandler(async (req: Request, res: Response) => {
   if (!req.body.name) {
     res.status(400);
     throw new Error("name is required");
   }
-  res.status(201).json({ message: "user created" });
-};
+  const userCreate = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    uuid: uuidV4(),
+  });
+  if (!userCreate) {
+    throw new Error("User failed to be created");
+  }
+  res.status(201).json({
+    message: "user created",
+    details: {
+      userCreate,
+    },
+  });
+});
 
 const deleteOneUser = (req: Request, res: Response) => {
   res.status(200).json({ message: `${req.params.id} deleted` });
---
+++ app/models/user-model.ts
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    uuid: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.models.User || mongoose.model("User", userSchema);
